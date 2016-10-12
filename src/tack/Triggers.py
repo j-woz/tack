d57a4f9494dae3d45762d9770a4a6351b7269940
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-10-12 11:23:08 -0500

    Use "kind" instead of "type"; better argument exceptions and defaults

--- src/tack/Triggers.py
+++ src/tack/Triggers.py
@@ -8,23 +8,23 @@ import time
 class TriggerFactory:
     def __init__(self, tack):
         self.tack = tack
-        self.types = { "timer":TimerTrigger }
+        self.kinds = { "timer":TimerTrigger, "process":ProcessTrigger }
 
     def new(self, **kwargs):
 
         print(kwargs)
 
         try:
-            t = kwargs["type"]
+            t = kwargs["kind"]
         except:
-            logging.critical("Given trigger with no type!")
+            logging.critical("Given trigger with no kind!")
             sys.exit(1)
 
-        if not t in self.types:
-            logging.critical("No such type: " + t)
+        if not t in self.kinds:
+            logging.critical("No such kind: " + t)
             sys.exit(1)
 
-        T = self.types[t]
+        T = self.kinds[t]
         result = T(self.tack, kwargs)
         self.tack.add_trigger(result)
         return result
@@ -34,6 +34,7 @@ class Trigger:
     def __init__(self, tack, args):
         self.tack = tack
         self.id = self.tack.make_id()
+        self.kind = "SUPER"
 
         try:
             self.name = args["name"]
@@ -45,6 +46,21 @@ class Trigger:
 
     def __str__(self):
         return "%s <%i>" % (self.name, self.id)
+
+    defaultDefault = object()
+
+    # d: a dictionary ; k: the key ; default: optional default value
+    def key(self, d, k, default=defaultDefault):
+        try:
+            result = d[k]
+        except KeyError:
+            if default is defaultDefault:
+                logging.critical("Given %s trigger with no %s!" %
+                                 (kind, k))
+                sys.exit(1)
+            else:
+                return default
+        return result
 
     def info(self, message):
         logging.info("%s: %s" % (str(self), message))
@@ -61,19 +77,33 @@ class Trigger:
     def shutdown(self):
         logging.info("Default shutdown(): %s" % str(self))
 
-
 class TimerTrigger(Trigger):
 
     def __init__(self, tack, args):
         super().__init__(tack, args)
-        self.interval = args["interval"]
-        logging.info("New TimerTrigger \"%s\" (%0.3fs)" %
+        self.interval = self.key(args, "interval", 0)
+        logging.info("New TimerTrigger \"%s\" (%0.3fs)" % \
                      (self.name, self.interval))
         self.last_poll = time.time()
+        self.handler = self.key(args, "handler")
+
+    def poll(self):
+        self.debug("poll()")
+        t = time.time()
+        if t - self.last_poll > self.interval:
+            self.debug("Calling handler")
+            self.handler(self, t)
+            last_poll = t
+
+class ProcessTrigger(Trigger):
+    def __init__(self, tack, args):
+        super().__init__(tack, args)
+        self.command = args["command"]
+        logging.info("New TimerTrigger \"%s\" (%s)" % (self.name, self.command))
         try:
             self.handler = args["handler"]
         except KeyError:
-            logging.critical("Given timer trigger with no handler!")
+            logging.critical("Given process trigger with no handler!")
             sys.exit(1)
 
     def poll(self):
